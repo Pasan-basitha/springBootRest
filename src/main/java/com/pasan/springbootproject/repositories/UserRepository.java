cada1d93f571281ae95fbf535c2e416dbf55aaeb
Pasanbashz <bashzpasan@gmail.com>
2021-06-04 21:23:57 +0530

    commited

--- src/main/java/com/pasan/springbootproject/repositories/UserRepository.java
+++ src/main/java/com/pasan/springbootproject/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDTO,Integer> {
 
-    //getAllUsers
+    //getAllUsers //
     //create
     //update
     //findById
